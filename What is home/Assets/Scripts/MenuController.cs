3d96a94d33253599e01c9ab60d38fac712157c58
Mykolas Kunevicius <saint667353@gmail.com>
2019-01-26 23:08:55 +0200

    Added Window and Door

---
+++ What is home/Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void onStartClick()
+    {
+        SceneManager.LoadScene("Testin");
+    }
+
+    public void onCreditsClick()
+    {
+
+    }
+
+    public void onExitClick()
+    {
+        Application.Quit();
+        Debug.Log("oof");
+    }
+}
