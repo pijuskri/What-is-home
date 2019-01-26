ce5325f4069f12e10f19fa7c8596370e37e556bc
Unknown <pijuskri@gmail.com>
2019-01-26 23:08:24 +0200

    ey

---
+++ What is home/Assets/GameLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    public GameObject player;
+    public Transform teleportPoint;
+    public Transform teleportFrom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, teleportFrom.position) < 10) ToSecretRoom();
+    }
+    public void ToSecretRoom()
+    {
+        Debug.Log("noi");
+        player.transform.position = teleportPoint.position;
+    }
+}
