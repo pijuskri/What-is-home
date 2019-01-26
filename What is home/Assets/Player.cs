01cbea14985b1e31b46f263dd17e69c2a0b0dfdd
Unknown <pijuskri@gmail.com>
2019-01-26 10:14:58 +0200

    finished inventory, working on cutting wood

--- What is home/Assets/Player.cs
+++ What is home/Assets/Player.cs
@@ -14,11 +14,13 @@ public class ItemDef
 {
     public string name;
     public string type;
+    public GameObject itemObject;
     public ItemDef(string name, string type)
     {
         this.name = name;
         this.type = type;
     }
+    public ItemDef() { }
 }
 public class Item
 {
@@ -29,21 +31,25 @@ public class Item
         this.ItemDef = itemDef;
         this.amount = amount;
     }
+    public Item()
+    {
+    }
 }
 public class Player : MonoBehaviour
 {
-    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
-    public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 4F;
-    public float sensitivityY = 4F;
-    public float minimumX = -360F;
-    public float maximumX = 360F;
-    public float minimumY = -70F;
-    public float maximumY = 70F;
+    #region camera
+    enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    RotationAxes axes = RotationAxes.MouseXAndY;
+    float sensitivityX = 4F;
+    float sensitivityY = 4F;
+    float minimumX = -360F;
+    float maximumX = 360F;
+    float minimumY = -70F;
+    float maximumY = 70F;
     float rotationY = 0F;
+    #endregion
 
-
-    // Start is called before the first frame update
+    #region publics in editor
     public GameObject playerObject;
     Rigidbody playerRigid;
     public GameObject itemDisplayPrefab;
@@ -51,10 +57,14 @@ public class Player : MonoBehaviour
     public GameObject InventoryPanel;
     public GameObject hotbarUI;
     public GameObject[] hotbarItems;
+    public GameObject[] itemObjectPrefabs;
+    [HideInInspector] public GameObject currentItemObject;
+    public Transform HandSocket;
+    #endregion
 
-    public Items items;
-    public List<Item> inventory;
-    public int[] hotbar = new int[9];
+    [HideInInspector] public Items items;
+    [HideInInspector] public List<Item> inventory;
+    [HideInInspector] public int[] hotbar = new int[9];
 
     public int currentItem = 0;
     float speed = 3f;
@@ -71,11 +81,14 @@ public class Player : MonoBehaviour
         playerRigid.freezeRotation = true;
 
         items = JsonUtility.FromJson<Items>(File.ReadAllText( Application.dataPath + "/items.json"));
+        InstantiateItems();
         inventory = new List<Item>();
         inventory.Add(new Item(items.items[0], 10));
         inventory.Add(new Item(items.items[1], 1));
         PopulateInventory();
         InventoryPanel.SetActive(false);
+        Cursor.visible = false;
+        hotbarItems[0].GetComponent<Image>().color = new Color(255, 255, 255, 0.7f);
     }
 
 
@@ -83,21 +96,38 @@ public class Player : MonoBehaviour
     {
         playerRigid.velocity = (Input.GetAxis("Horizontal") * playerObject.transform.right + Input.GetAxis("Vertical") * playerObject.transform.forward) * speed;
         if(!invOpen)CameraLook();
+        transform.position = new Vector3(playerObject.transform.position.x + 0.3f, playerObject.transform.position.y + 0.5f, playerObject.transform.position.z);
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (invOpen) { invOpen = false; InventoryPanel.SetActive(false); }
-            else { invOpen = true; InventoryPanel.SetActive(true); }
+            if (invOpen) { invOpen = false; InventoryPanel.SetActive(false); Cursor.visible = false; }
+            else { invOpen = true; InventoryPanel.SetActive(true); Cursor.visible = true; }
         }
         UpdateHotbar();
-        if (NumKey() != -1)
+        if (NumKey() != -1 && !invOpen)
         {
+            hotbarItems[currentItem].GetComponent<Image>().color = new Color(255, 255, 255, 0.39f);
             currentItem = NumKey();
+            hotbarItems[currentItem].GetComponent<Image>().color = new Color(255,255,255,0.7f);
+            Debug.Log(hotbarItems[currentItem].GetComponent<Image>().color);
+            UpdateHand();
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (inventory[hotbar[currentItem]].ItemDef.name == "Axe")
+            {
+                if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit))
+                {
+                    if (hit.collider.gameObject.tag == "Tree")
+                    {
+                        AddItemToInventory(new Item(FindItemByName("Wood"), 1));
+                    }
+                }
+            }
         }
     }
     void CameraLook()
     {
-        transform.position = new Vector3(playerObject.transform.position.x, playerObject.transform.position.y + 0.5f, playerObject.transform.position.z);
         if (axes == RotationAxes.MouseXAndY)
         {
             float rotationX = transform.eulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
@@ -121,6 +151,24 @@ public class Player : MonoBehaviour
         }
         playerObject.transform.rotation = Quaternion.Euler(playerObject.transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, playerObject.transform.rotation.eulerAngles.z);
     }
+    void InstantiateItems()
+    {
+        int index = 0;
+        foreach (var item in items.items)
+        {
+            item.itemObject = itemObjectPrefabs[index];
+            index++;
+        }
+    }
+    void AddItemToInventory(Item itemAdd)
+    {
+        foreach (var item in inventory)
+        {
+            if (item.ItemDef == itemAdd.ItemDef) { item.amount += itemAdd.amount; return; }
+        }
+        inventory.Add(itemAdd);
+    }
+    #region UI
     void PopulateInventory()
     {
         int index = 0;
@@ -151,6 +199,14 @@ public class Player : MonoBehaviour
             index++;
         }
     }
+    void UpdateHand()
+    {
+        Destroy(currentItemObject);
+        currentItemObject = null;
+        if(hotbar[currentItem]!=-1)currentItemObject = Instantiate(inventory[hotbar[currentItem]].ItemDef.itemObject, HandSocket);
+    }
+    #endregion
+    #region Utils
     public static int NumKey()
     {
         int num = -1;
@@ -166,4 +222,14 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha9)) num = 8;
         return num;
     }
+    public ItemDef FindItemByName(string name)
+    {
+        ItemDef itemResult = new ItemDef();
+        foreach (var item in items.items)
+        {
+            if (item.name == name) { itemResult = item;break; }
+        }
+        return itemResult;
+    }
+    #endregion
 }
